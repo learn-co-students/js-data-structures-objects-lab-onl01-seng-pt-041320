cd962bf6cc211242562c922796aca6038bac113f
Dice-K39 <daisukehashimoto39@gmail.com>
2020-10-24 15:17:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) 
+{
+    const driverCopy = { ...driver };
+    
+    driverCopy[key] = value;
+
+    return driver, driverCopy;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+{
+    driver[key] = value;
+
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key)
+{
+    const driverCopy = { ...driver };
+
+    delete driverCopy[key];
+
+    return driverCopy;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key)
+{
+    delete driver[key];
+
+    return driver;
+}
