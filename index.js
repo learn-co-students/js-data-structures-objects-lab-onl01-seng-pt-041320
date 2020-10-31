5f4d9c350d45e1925162c551c162f572914ef532
marcusLau <marcus.lau76@gmail.com>
2020-10-30 17:57:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let driver = {}
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+    return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    obj[key] = value // changes value at key
+    return obj
+}
+
+// Non-destructive way of removing a key from obj.
+function deleteFromDriverByKey(obj, key) { // key is "name"
+    let clone = Object.assign({}, obj) // clones into an empty object from obj
+    delete clone[key] // has to be in brackets cuz it looks inside Obj for an attr named "key"
+    return clone
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
