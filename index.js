dd7214b4a8ae9c5aef8e43edf83f2a17164562ef
christinezosche <christine.zosche@gmail.com>
2020-11-02 16:46:06 -0800

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(obj, key, value) {
-    const newObj = { ...obj };
- 
-    newObj[key] = value;
- 
-    return newObj;
+    return Object.assign({}, obj, { [key]: value });
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
